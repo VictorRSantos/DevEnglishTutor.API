23584545a8e358192267d8045d8bd03dab25e358
Victor Rodrigues <victor.rsantos@outlook.com>
2023-02-25 14:16:01 -0300

    Add project files.

---
+++ DevEnglishTutor.API/Controllers/EnglishTutorController.cs
@@ -0,0 +1,48 @@
+﻿using DevEnglishTutor.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DevEnglishTutor.API.Controllers
+{
+    [ApiController]
+    [Route("api/english-tutor")]
+    public class EnglishTutorController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public EnglishTutorController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // api/english-tutor?text=
+        [HttpGet]
+        public async Task<IActionResult> Get(string text, [FromServices] IConfiguration configuration)
+        {
+            // Pegar o Token
+            var token = configuration.GetValue<string>("ChatGptSecretKey");
+            // Autorização
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var model = new ChatGptInputModel(text);
+
+            var requestBody = JsonSerializer.Serialize(model);
+
+            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://api.openai.com/v1/completions", content);
+
+            var result = await response.Content.ReadFromJsonAsync<ChatGptViewModel>();
+
+            var promptResponse = result?.choices.First();
+
+
+            return Ok(promptResponse?.text?.Replace("\n","").Replace("\t",""));
+        }
+
+
+
+    }
+}
